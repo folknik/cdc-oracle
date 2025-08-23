5d0a81825cb200f7c60b328a6ed4a21a79bfb900
Alexey Fadeev <alexey.fadeev@easypay.team>
2025-08-23 11:39:53 +0300

    update file

--- 4.build_oracle_image.sh
+++ 4.build_oracle_image.sh
@@ -2,7 +2,7 @@
 
 echo "1. build Oracle 21c XE"
 cd /home/oracle-docker/OracleDatabase/SingleInstance/dockerfiles
-bash ./buildDockerImage.sh -v 21.3.0 -x
+bash ./buildContainerImage.sh -v 21.3.0 -x
 
 
 echo "- all OK"
