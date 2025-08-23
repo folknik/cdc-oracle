e5cb83adc9f2033bc9c8c4b7c763f61a5e6a0bab
Alexey Fadeev <alexey.fadeev@easypay.team>
2025-08-23 11:53:12 +0300

    update file

--- 5.build_olr_image.sh
+++ 5.build_olr_image.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "1. build OpenLogReplicator v1.3.0"
+echo "1. create user for OpenLogReplicator installation"
+sudo adduser olruser
+sudo usermod -aG docker olruser
+su - olruser
+
+
+echo "2. build OpenLogReplicator v1.3.0"
 cd /home/openlogreplicator-docker
 bash ./build-prod.sh
+echo "- build OpenLogReplicator OK"
 
 
 echo "- all OK"
