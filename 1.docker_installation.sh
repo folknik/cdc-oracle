74e805828f704e46ee6836819b31100248211abf
Alexey Fadeev <alexey.fadeev@easypay.team>
2025-08-23 19:46:13 +0300

    update file

--- 1.docker_installation.sh
+++ 1.docker_installation.sh
@@ -17,10 +17,4 @@ sudo usermod -aG docker root
 echo "- Executing the Docker Command Without Sudo OK"
 
 
-echo "Step 3 — Installing Docker Compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo "- Installing Docker Compose OK"
-
-
 echo "- ALL OK"
