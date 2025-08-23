b584fb3afb0027fc5898679d9e6394826ab3c2f3
Alexey Fadeev <alexey.fadeev@easypay.team>
2025-08-23 21:19:00 +0300

    update file

--- 9.grants.sh
+++ 9.grants.sh
@@ -20,7 +20,82 @@ set -e
 
 . cfg.sh
 
-echo "4.- creating database schema"
-sql /opt/sql/schema.sql /opt/sql/schema.out
+echo "1.- Create Log Miner Tablespace in CDB database"
+docker exec ${DB_CONTAINER} /bin/bash -c "export NLS_LANG=american_america.AL32UTF8
+export ORACLE_SID=XE
+. oraenv
+sqlplus sys/123@//localhost:1521/XE as sysdba <<- EOF
+  CREATE TABLESPACE LOGMINER_TBS DATAFILE '/opt/oracle/oradata/XE/logminer_tbs.dbf' SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
+  exit;
+EOF
+"
+
+
+echo "2.- Create Log Miner Tablespace in PDB database"
+docker exec ${DB_CONTAINER} /bin/bash -c "export NLS_LANG=american_america.AL32UTF8
+export ORACLE_SID=XE
+. oraenv
+sqlplus sys/123@//localhost:1521/XEPDB1 as sysdba <<- EOF
+  CREATE TABLESPACE LOGMINER_TBS DATAFILE '/opt/oracle/oradata/XE/XEPDB1/logminer_tbs.dbf' SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
+  exit;
+EOF
+"
+
+echo "3.- Create common user and its rights"
+docker exec ${DB_CONTAINER} /bin/bash -c "export NLS_LANG=american_america.AL32UTF8
+export ORACLE_SID=XE
+. oraenv
+sqlplus sys/123@//localhost:1521/XE as sysdba <<- EOF
+  CREATE USER c##dbzuser IDENTIFIED BY dbz DEFAULT TABLESPACE LOGMINER_TBS QUOTA UNLIMITED ON LOGMINER_TBS CONTAINER=ALL;
+
+  GRANT CREATE SESSION TO c##dbzuser CONTAINER=ALL;
+  GRANT SET CONTAINER TO c##dbzuser CONTAINER=ALL;
+  GRANT SELECT ON V_\$DATABASE TO c##dbzuser CONTAINER=ALL;
+  GRANT SELECT ANY DICTIONARY TO c##dbzuser CONTAINER=ALL;
+  GRANT LOCK ANY TABLE TO c##dbzuser CONTAINER=ALL;
+  -- These can be reduced from ANY TABLE to your captured tables depending on your security model
+  GRANT SELECT ANY TABLE TO c##dbzuser CONTAINER=ALL;
+  GRANT FLASHBACK ANY TABLE TO c##dbzuser CONTAINER=ALL;
+
+  -- OpenLogReplicator specific permissions
+  ALTER SESSION SET CONTAINER = ORCLPDB1;
+  GRANT SELECT, FLASHBACK ON SYS.CCOL$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.CDEF$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.COL$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.DEFERRED_STG$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.ECOL$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.LOB$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.LOBCOMPPART$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.LOBFRAG$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.OBJ$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.TAB$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.TABCOMPART$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.TABPART$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.TABSUBPART$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.TS$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON SYS.USER$ TO c##dbzuser;
+  GRANT SELECT, FLASHBACK ON XDB.XDB$TTSET TO c##dbzuser;
+
+  DECLARE
+  CURSOR C1 IS SELECT TOKSUF FROM XDB.XDB$TTSET;
+  CMD VARCHAR2(2000);
+  BEGIN
+  FOR C IN C1 LOOP
+          CMD := 'GRANT SELECT, FLASHBACK ON XDB.X$NM' || C.TOKSUF || ' TO c##dbzuser';
+  EXECUTE IMMEDIATE CMD;
+  CMD := 'GRANT SELECT, FLASHBACK ON XDB.X$QN' || C.TOKSUF || ' TO c##dbzuser';
+  EXECUTE IMMEDIATE CMD;
+  CMD := 'GRANT SELECT, FLASHBACK ON XDB.X$PT' || C.TOKSUF || ' TO c##dbzuser';
+  EXECUTE IMMEDIATE CMD;
+  END LOOP;
+  END;
+  /
+
+  ALTER SESSION SET CONTAINER = CDB$ROOT;
+  ALTER SYSTEM ARCHIVE LOG CURRENT;
+
+  exit;
+EOF
+"
 
 echo "- all OK"
