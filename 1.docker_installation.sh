62227c8007e563d1b2e5912f39b97029324db9be
Alexey Fadeev <alexey.fadeev@easypay.team>
2025-08-23 11:03:03 +0300

    update file

--- 1.docker_installation.sh
+++ 1.docker_installation.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Step 1 — Installing Docker
+echo "Step 1 — Installing Docker"
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
+echo "- Installing Docker OK"
 
 
-# Step 2 — Executing the Docker Command Without Sudo (Optional)
+echo "Step 2 — Executing the Docker Command Without Sudo"
 sudo usermod -aG docker ${USER}
 su - ${USER}
 sudo usermod -aG docker root
+echo "- Executing the Docker Command Without Sudo OK"
 
 
-# Step 3 — Installing Docker Compose
+echo "Step 3 — Installing Docker Compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+echo "- Installing Docker Compose OK"
 
-echo "- all OK"
+
+echo "- ALL OK"
