d20828ffbbd7b47f3603fe6585d7357302714030
Alexey Fadeev <alexey.fadeev@easypay.team>
2025-08-26 10:37:10 +0300

    update file

--- 11.create_common_user.sh
+++ 11.create_common_user.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
 
-. cfg.sh
+export DB_CONTAINER=${DB_CONTAINER:=ORA1}
 
-docker exec ${DB_CONTAINER} /bin/bash -c "
-  export NLS_LANG=american_america.AL32UTF8
-  export ORACLE_SID=XE
-  . oraenv
 
-  sqlplus sys/123@//localhost:1521/XE as sysdba <<- 'EOF'
-    CREATE USER c##dbzuser IDENTIFIED BY dbz
-      DEFAULT TABLESPACE LOGMINER_TBS
-      QUOTA UNLIMITED ON LOGMINER_TBS
-      CONTAINER=ALL;
+sql() {
+    docker exec ${DB_CONTAINER} /bin/bash -c "export NLS_LANG=american_america.AL32UTF8
+export ORACLE_SID=XE
+. oraenv
+sqlplus / as sysdba <<- EOF
+set echo off
+set verify off
+set heading off
+set termout off
+set showmode off
+set linesize 5000
+set pagesize 0
+@${1}
+EOF
+" 1>/dev/null 2>&1
+}
 
-    GRANT CREATE SESSION TO c##dbzuser CONTAINER=ALL;
-    GRANT SET CONTAINER TO c##dbzuser CONTAINER=ALL;
-    GRANT SELECT ON V_$DATABASE TO c##dbzuser CONTAINER=ALL;
-    GRANT SELECT ANY DICTIONARY TO c##dbzuser CONTAINER=ALL;
-    GRANT LOCK ANY TABLE TO c##dbzuser CONTAINER=ALL;
 
-    GRANT SELECT ANY TABLE TO c##dbzuser CONTAINER=ALL;
-    GRANT FLASHBACK ANY TABLE TO c##dbzuser CONTAINER=ALL;
-    GRANT SELECT_CATALOG_ROLE TO c##dbzuser CONTAINER=ALL;
-    GRANT EXECUTE_CATALOG_ROLE TO c##dbzuser CONTAINER=ALL;
-    GRANT SELECT ANY TRANSACTION TO c##dbzuser CONTAINER=ALL;
+sql /opt/sql/11.create_common_user.sql
 
-    EXIT;
-  EOF
-"
 
 echo "- all OK"
